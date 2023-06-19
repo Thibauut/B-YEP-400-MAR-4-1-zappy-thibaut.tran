eecd852da387368630fe640070b46a98c34bbaf0
Zyad <zyad.othman@epitech.eu>
2023-06-19 21:32:37 +0200

    IA 0.7 test level 5

--- ai/ai.py
+++ ai/ai.py
@@ -47,9 +47,123 @@ class MyClient():
                 if descriptor == sys.stdin:
                     self.send_command_to_server()
                 else:
+<<<<<<< HEAD
                     self.receive_data_from_server()
             if self.data.decode() == "WELCOME\n":
                getWelcome()
+=======
+                    client.send_command_to_server(f"Take {item_to_take}\n")
+                    client.receive_data_from_server()
+            elif item_to_take == 'food':
+                if item_to_take in self.look[2] or self.rotation >= 3:
+                    client.send_command_to_server("Forward\n")
+                    self.rotation = 0
+                else:
+                    client.send_command_to_server("Right\n")
+                    self.rotation += 1
+                client.receive_data_from_server()
+        else:
+            client.send_command_to_server("Forward\n")
+            client.receive_data_from_server()
+
+    def incantation(self, client):
+        self.linemate = self.get_item("linemate")
+        self.deraumere = self.get_item("deraumere")
+        self.sibur = self.get_item("sibur")
+        self.mendiane = self.get_item("mendiane")
+        self.phiras = self.get_item("phiras")
+        self.thystame = self.get_item("thystame")
+        self.level2(client)
+        self.level3(client)
+        self.level4(client)
+        self.level5(client)
+
+    def level2(self, client):
+        if self.level == 1 and self.linemate >= 1:
+            client.send_command_to_server("Set linemate\n")
+            client.receive_data_from_server()
+            client.send_command_to_server("Incantation\n")
+            client.receive_data_from_server()
+            if (client.data.decode() != "ko\n"):
+                client.receive_data_from_server()
+                if (client.data.decode() != "ko\n"):
+                    self.level += 1
+
+    def level3(self, client):
+        if (
+            self.level == 2 and
+            self.look[0].count('player') >= 2 and
+            self.linemate >= 1 and
+            self.deraumere >= 1 and
+            self.sibur >= 1
+        ):
+            client.send_command_to_server("Set linemate\n")
+            client.receive_data_from_server()
+            client.send_command_to_server("Set deraumere\n")
+            client.receive_data_from_server()
+            client.send_command_to_server("Set sibur\n")
+            client.receive_data_from_server()
+            client.send_command_to_server("Incantation\n")
+            client.receive_data_from_server()
+            if (client.data.decode() != "ko\n"):
+                client.receive_data_from_server()
+                if (client.data.decode() != "ko\n"):
+                    self.level += 1
+                    client.receive_data_from_server()
+
+    def level4(self, client):
+        if (
+            self.level == 3 and
+            self.look[0].count('player') >= 2 and
+            self.linemate >= 2 and
+            self.phiras >= 2 and
+            self.sibur >= 1
+        ):
+            client.send_command_to_server("Set linemate\n")     # PK y'a deux Set linemate ic
+            client.receive_data_from_server()
+            client.send_command_to_server("Set linemate\n")     # et ici
+            client.receive_data_from_server()
+            client.send_command_to_server("Set phiras\n")
+            client.receive_data_from_server()
+            client.send_command_to_server("Set phiras\n")
+            client.receive_data_from_server()
+            client.send_command_to_server("Set sibur\n")
+            client.receive_data_from_server()
+            client.send_command_to_server("Incantation\n")
+            client.receive_data_from_server()
+            if (client.data.decode() != "ko\n"):
+                client.receive_data_from_server()
+                if (client.data.decode() != "ko\n"):
+                    self.level += 1
+                    client.receive_data_from_server()
+
+    def level5(self, client):
+        if (
+            self.level == 4 and
+            self.look[0].count('player') >= 4 and
+            self.linemate >= 1 and
+            self.deraumere >= 1 and
+            self.phiras >= 1 and
+            self.sibur >= 2
+        ):
+            client.send_command_to_server("Set linemate\n")
+            client.receive_data_from_server()
+            client.send_command_to_server("Set linemate\n")
+            client.receive_data_from_server()
+            client.send_command_to_server("Set phiras\n")
+            client.receive_data_from_server()
+            client.send_command_to_server("Set phiras\n")
+            client.receive_data_from_server()
+            client.send_command_to_server("Set sibur\n")
+            client.receive_data_from_server()
+            client.send_command_to_server("Incantation\n")
+            client.receive_data_from_server()
+            if (client.data.decode() != "ko\n"):
+                client.receive_data_from_server()
+                if (client.data.decode() != "ko\n"):
+                    self.level += 1
+                    client.receive_data_from_server()
+>>>>>>> 7f5cd87 (IA 0.7 test level 5)
 
 def getWelcome():
     print("Is Welcomed")
